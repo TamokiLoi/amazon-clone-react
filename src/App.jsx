41b4611af49b4250532f45023465c9e3df9c2d84
loinlt <loinlt@stageid.app>
2021-04-06 16:07:07 +0700

    init src

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Checkout from "./components/checkout/Checkout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+	return (
+		<Router>
+			<div className="App">
+				<Header />
+				<Switch>
+					<Route path="/checkout">
+						<Checkout />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBNvTGFU3k7RSr8_jRUnkjLxJLIfvNcjj8",
+    authDomain: "clone-a437c.firebaseapp.com",
+    projectId: "clone-a437c",
+    storageBucket: "clone-a437c.appspot.com",
+    messagingSenderId: "1001751473942",
+    appId: "1:1001751473942:web:67460dcefbad35af1c6a37",
+    measurementId: "G-TE8ZF921NK"
+};
